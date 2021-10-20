93dca0c15ccfe00e5171a34bb4dc335a39d421b9
Max Vorobyev <gtvolk31@gmail.com>
2021-10-21 02:07:40 +0300

    SonarLint code cleaning

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 
 group = "ru.devvault.client.vk"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_1_8
+java.sourceCompatibility = JavaVersion.VERSION_11
 
 configurations {
 	compileOnly {
