ad0e7eef3e12c115e3f54585d68de945af69416a
Max Vorobyev <GTVolk@users.noreply.github.com>
2021-10-21 01:38:45 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "2.5.4"
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
-	id "org.sonarqube" version "3.3"
+	id("org.sonarqube") version "3.3"
 	kotlin("jvm") version "1.5.21"
 	kotlin("plugin.spring") version "1.5.21"
 }
@@ -16,6 +16,12 @@ configurations {
 	compileOnly {
 		extendsFrom(configurations.annotationProcessor.get())
 	}
+}
+
+sonarqube {
+  properties {
+    property "sonar.projectKey", "GTVolk_VkPoster"
+  }
 }
 
 repositories {
@@ -46,10 +52,4 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
-}
-
-sonarqube {
-  properties {
-    property "sonar.projectKey", "GTVolk_VkPoster"
-  }
 }
